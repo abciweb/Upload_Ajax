1f9fc087042e38303257633f717d3b0843741c9a
Alain Bontemps <dev@abciweb.net>
2015-03-03 00:17:11 +0100

    Create UploadAjaxABCI_Php_Load_Basique.php

---
+++ UploadAjaxABCI_Php_Load_Basique.php
@@ -0,0 +1,31 @@
+<?php
+//UploadAjaxABCI_Php_Load_Basique Version 2.4
+
+session_start();
+// Voir le fichier "UploadAjaxABCI_Php_Load.php" pour plus d'exemples détaillés.
+
+// maximise les possibilités de reprise d'upload en cas d'arrêt intempestif.
+ignore_user_abort(true);
+
+header('Content-type: text/html; charset=UTF-8');
+
+
+// Charge la classe php d'upload
+require 'UploadAjaxABCIServeur.php';
+
+//Initialisation de la classe php (dossier de destination, dossier temporaire)
+//CHANGER LES NOMS OU CREER LES DOSSIERS PASSES EN PARAMETRE
+$up = new UploadAjaxABCIServeur('Destination_Upload/', 'Upload_Temp/');
+
+// getUniqidForm() renvoie l'identifiant de formulaire pour la vérification ci-dessous
+$unidid_form = $up->getUniqidForm();
+
+// Il est conseillé de ne pas supprimer cette ligne car c'est le token qui assure que ce script est appelé depuis le formulaire
+if(!(isset($unidid_form,$_SESSION['UploadAjaxABCI'][$unidid_form]))) $up->exitErreurFichier('Identifiant de formulaire non valide. Rafraîchissez la page'); 
+
+$up->Upload();
+// Le processus d'upload est dissocié de celui du transfert, pour permettre de traiter le fichier complet (par exemple pour faire des redimensionnements d'images en php etc.) avant de le déplacer vers son emplacement définitif (c.f "UploadAjaxABCI_Php_Load.php" pour des exemples). 
+$up->Transfert();
+
+$up->exitReponseAjax();
+?>
